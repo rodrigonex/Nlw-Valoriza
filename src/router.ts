7f93f39f1294e1da459f85dbfe87a9154694094d
Your Name <rodrigoe923@gmail.com>
2021-07-06 11:14:12 -0300

    fim dia 4

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './Controllers/AuthenticateUserController';
+import { CreateComplimentController } from './Controllers/CreateComplimentController';
 import { CreateTagController } from './Controllers/CreateTagController';
 import { CreateUserController } from './Controllers/CreateUserController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
@@ -10,10 +11,12 @@ const router = Router();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController(); 
 const authenticateUserController = new AuthenticateUserController(); 
+const createComplimentController = new CreateComplimentController();
 
 router.post("/tags", ensureAdmin, createTagController.handle);
 router.post("/users", createUserController.handle);
 router.post("/login", authenticateUserController.handle);
+router.post("/complimets", createComplimentController.handle);
 
 
 export { router };
